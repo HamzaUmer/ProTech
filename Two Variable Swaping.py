9af3d269557c15582f02e351b31e1174ec74bc95
Mirza Hamza <mirzasahab123321@gmail.com>
2019-06-17 16:01:45 +0500

    Swaping

---
+++ Two Variable Swaping.py
@@ -0,0 +1,7 @@
+x=int(input("Value of x = "))
+y=int(input("Value of y = "))
+temp=x
+x=y
+y=temp
+print("The value of x after swaping = {}".format(x))
+print("The value of y after swaping = {}".format(y))
