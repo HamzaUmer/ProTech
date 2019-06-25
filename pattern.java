05469d5ba0c2249a657f2d69c840494f1561f129
Mirza Hamza <mirzasahab123321@gmail.com>
2019-06-25 20:25:45 +0500

    * pattern

---
+++ pattern.java
@@ -0,0 +1,22 @@
+class pattern
+{
+public static void main(String arg[])
+{
+int a,b;
+for(a=5;a>=1;a--)
+{
+for(b=1;b<=a;b++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
+OUTPUT:
+E:\jdk1.7\bin>java pattern
+*****
+****
+***
+**
+*
