9ff037a4380aa1a4f126c55b80deac8687251418
Mirza Hamza <mirzasahab123321@gmail.com>
2019-06-26 19:35:37 +0500

    Odd number generator

---
+++ odd.java
@@ -0,0 +1,24 @@
+class odd4
+{
+public static void main(String arg[])
+{
+int a=37;
+do
+{
+System.out.println(a);
+a-=2;
+}while(a>=19);
+}
+}
+Output:
+E:\jdk1.7\bin>java odd4
+37
+35
+33
+31
+29
+27
+25
+23
+21
+19
